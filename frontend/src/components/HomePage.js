64094b48f9381c7df9d7442c4edf5220db1e71a0
Ghadi <ghadi.j.alkehily@gmail.com>
2024-04-20 18:03:11 +0300

    homepage

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -6,6 +6,25 @@ const HomePage = () => {
     return (
         <div className="homepage">
             <Header />
+            <div className="label-container">
+                <p className="label-text"> مرحبا، أنا 
+                <span> نديم  </span>
+                <br></br> رفيق القراءة! </p>
+            </div>
+            <div className="label-container2">
+            <p className='lbl2'>
+                لدينا قصص بمختلف التصنيفات
+                </p>
+                </div>
+                <div className="label-container3">
+                <p className='lbl3'>
+                    <span className='span1'>في <span className='span2'> نديم </span></span>
+                    <br></br>
+                    <span className='span3'>
+نسافر بخيالنا في رحلات ممتعة في
+<br></br> مجرات من المرح وبآفاق العلم والأخلاق</span></p>
+ </div>
+
         </div>
     );
 };
